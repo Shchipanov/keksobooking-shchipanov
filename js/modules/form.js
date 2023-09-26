99f879629ac6631578582157f658e44e09c059ed
Shchipanov <chips_80@mail.ru>
2023-09-26 14:55:57 +0400

    feat: делает валидацию

--- js/modules/form.js
+++ js/modules/form.js
@@ -1,4 +1,44 @@
 /** Перевод формы в неактивное состояние */
-export const makesDisabledForm = () => {
-  document.querySelector('.ad-form').classList.add('ad-form--disabled');
+const formElement = document.querySelector('.ad-form');
+const fieldsetElements = formElement.querySelectorAll('fieldset');
+
+// TODO Слайдер также должен быть заблокирован - он в fieldset. Дополнительная блокирвока по классу ad-form__slider не сделана.
+/** Перевод формы в неактивное состояние */
+const disableForm = () => {
+  formElement.classList.add('ad-form--disabled');
+  for (const fieldElement of fieldsetElements) {
+    fieldElement.disabled = true;
+  }
 };
+
+/** Перевод формы в активное состояние */
+// TODO временное отключение проверки
+// eslint-disable-next-line no-unused-vars
+const enableForm = () => {
+  formElement.classList.remove('ad-form--disabled');
+  for (const fieldElement of fieldsetElements) {
+    fieldElement.disabled = false;
+  }
+};
+
+/** Блокировка формы с фильтрами */
+const filterElement = document.querySelector('.map__filters');
+const selectElements = filterElement.querySelectorAll('select');
+const disableMapFilters = () => {
+  filterElement.classList.add('map__filters--disabled');
+  for (const selectElement of selectElements) {
+    selectElement.disabled = true;
+  }
+};
+
+/** Разблокировка формы с фильтрами */
+// TODO временное отключение проверки
+// eslint-disable-next-line no-unused-vars
+const enableMapFilters = () => {
+  filterElement.classList.remove('map__filters--disabled');
+  for (const selectElement of selectElements) {
+    selectElement.disabled = false;
+  }
+};
+
+export {disableForm, disableMapFilters};
