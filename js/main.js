f893c2c3e44a6a067aeb60b5ba7617ee756e6c19
Shchipanov <chips_80@mail.ru>
2023-09-29 16:07:23 +0400

    feat завершает module8-task2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-import constants from './constants.js';
-import { generateArrayAdvertisments } from './modules/generate-advertisement.js';
-import { createCardElement,renderCard } from './modules/popup.js';
+// import constants from './constants.js';
+// import { generateArrayAdvertisments } from './modules/generate-advertisement.js';
+// import { createCardElement,renderCard } from './modules/popup.js';
 // import {disableForm, disableMapFilters} from './form.js';
-import './modules/validation.js';
+// import './modules/validation.js';
 
 //вызываем функцию генерации данных
-const advertisments = generateArrayAdvertisments(constants.LIMITED_NUMBER_ADVERTISEMENT);
+// const advertisments = generateArrayAdvertisments(constants.LIMITED_NUMBER_ADVERTISEMENT);
 
 /** Создание карточки */
-const cardElements = advertisments.map(createCardElement);
+// const cardElements = advertisments.map(createCardElement);
 
 /** Отрисовать на карте 1 карточку */
-renderCard(cardElements[0]);
+// renderCard(cardElements[0]);
 
 /**При открытии страница находится в неактивном состоянии */
 // disableForm();
@@ -22,10 +22,7 @@ renderCard(cardElements[0]);
 
 
 const map = L.map('map-canvas')
-  .on('load', () => {
-  // eslint-disable-next-line no-console
-    console.log('Карта инициализирована');
-  })
+
   .setView({
     lat: 35.68952,
     lng: 139.69199,
